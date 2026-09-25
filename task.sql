c52adb227fefe67b1dfba0f8ee69a1f25b6826bc
Khrystyna <tkachristina079@gmail.com>
2026-09-26 01:31:46 +0300

    solution

--- task.sql
+++ task.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS ShopDB;
+
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
@@ -15,8 +17,8 @@ CREATE TABLE Products (
 
 CREATE TABLE Warehouses (
     ID INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50),
+    Name VARCHAR(50),
+    Address VARCHAR(50),
     CountryID INT,
     PRIMARY KEY (ID),
     FOREIGN KEY (CountryID)
@@ -27,8 +29,8 @@ CREATE TABLE Warehouses (
 CREATE TABLE ProductInventory (
     ID INT,
     ProductID INT,
-    WarehouseID INT,
     WarehouseAmount INT,
+    WarehouseID INT,
     PRIMARY KEY (ID),
     FOREIGN KEY (ProductID)
         REFERENCES Products(ID)
@@ -37,8 +39,6 @@ CREATE TABLE ProductInventory (
         REFERENCES Warehouses(ID)
         ON DELETE NO ACTION
 );
-
--- Populate test data
 
 INSERT INTO Countries (ID, Name)
 VALUES (1, 'Country1');
@@ -49,14 +49,14 @@ VALUES (2, 'Country2');
 INSERT INTO Products (ID, Name)
 VALUES (1, 'AwersomeProduct');
 
-INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID)
+INSERT INTO Warehouses (ID, Name, Address, CountryID)
 VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1);
 
-INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID)
+INSERT INTO Warehouses (ID, Name, Address, CountryID)
 VALUES (2, 'Warehouse-2', 'City-2, Street-2', 2);
 
-INSERT INTO ProductInventory (ID, ProductID, WarehouseID, WarehouseAmount)
-VALUES (1, 1, 1, 2);
+INSERT INTO ProductInventory (ID, ProductID, WarehouseAmount, WarehouseID)
+VALUES (1, 1, 2, 1);
 
-INSERT INTO ProductInventory (ID, ProductID, WarehouseID, WarehouseAmount)
-VALUES (2, 1, 2, 5);
+INSERT INTO ProductInventory (ID, ProductID, WarehouseAmount, WarehouseID)
+VALUES (2, 1, 5, 2);
